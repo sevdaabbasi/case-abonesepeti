2e2502ef2ba63a97fe04e27866f0c59a9054eae9
Sevda Abbasi <sevdaabbasi37@gmail.com>
2025-10-30 10:52:57 +0300

    editing

--- Auth.Api/DTOs/Responses/LoginResponse.cs
+++
@@ -1,7 +0,0 @@
-namespace Auth.Api.Dtos.Responses;
-
-public record LoginResponse(
-    string AccessToken, 
-    string RefreshToken, 
-    DateTime AccessTokenExpiresAt, 
-    DateTime RefreshTokenExpiresAt);
